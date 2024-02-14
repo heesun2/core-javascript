d3aa111f50464efbce1647f2e798248c4e4f500d
02.dom <designh2sun@gmail.com>
2024-02-14 23:38:56 +0900

    animation

--- client/main.js
+++ client/main.js
@@ -1,11 +1,20 @@
-// const params = {
-//     host:'localhost',
-//     port:3000,
-//     open:false,
-//     root:'./client'
-// }
-
-// server.start(params);
 
 
-console.log(global)
+// import { diceAnimation } from './lib/animation/dice.js';
+import { diceAnimation, getNodes } from './lib/index.js';
+
+const [rollingbutton, recordButton, resetButton ] = getNodes('buttonGroup > button');
+
+let isClicked = false;
+
+function handleRollingDice(){
+
+    if(!isClicked){
+    stopAnimation = setInterval(diceAnimation, 100);
+    }else{
+        clearInterval(stopAnimation);
+    } 
+    isClicked = !isClicked;
+}
+
+rollingbutton.addEventListener('click',handleRollingDice);
