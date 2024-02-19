1982adac7649ab16438588b0969d4362137e5e63
02.dom <designh2sun@gmail.com>
2024-02-19 14:32:17 +0900

    learn, localStorage

--- client/main.js
+++ client/main.js
@@ -1,11 +1,44 @@
-// const params = {
-//     host:'localhost',
-//     port:3000,
-//     open:false,
-//     root:'./client'
-// }
+import { 
+  setStorage,
+  getStorage,
+  deleteStorage,
+  getNode,
+  clearContents
+} from './lib/index.js '
 
-// server.start(params);
+//1. textField의 값을 로컬스토리지에 저장 해 주세요
+// input, addEvent... eventType:input
 
 
-console.log(global)
+const textField = getNode('#textField');
+const clear = getNode('[data-name="clear"]')
+
+const handleTextField = (e) => {
+  const value = e.currentTarget.value;
+  setStorage('text', value);
+}
+
+
+const handleClear = () => deleteStorage('text').then(clearContents(textField))
+  
+
+textField.addEventListener('input',handleTextField);
+clear.addEventListener('click',handleClear);
+
+
+
+function init(){
+  getStorage('text').then((res)=>{
+    textField.value = res;
+  })
+}
+
+
+window.addEventListener('DOMContentLoaded', init);
+
+
+( async ()=>{
+  const data = await getStorage('text')
+  textField.value = data;
+})()
+
